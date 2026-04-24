9098007b8b0818b588882219b16015dbad1db093
Baturalp Yavuz <yavuz_baturalp@hacettepe.edu.tr>
2026-04-24 23:15:40 +0300

    aludec.sv

--- aludec.sv
+++ aludec.sv
@@ -9,15 +9,15 @@ module aludec(input logic opb5,
 
     always_comb
         case(ALUOp)
-            2'b00: ALUControl = 3'b000; // addition (ALU'da 000)
-            2'b01: ALUControl = 3'b001; // subtraction (ALU'da 001)
+            2'b00: ALUControl = 3'b000; // addition 
+            2'b01: ALUControl = 3'b001; // subtraction 
             default: case(funct3) // R-type or I-type ALU
                         3'b000: if (RtypeSub)
                                     ALUControl = 3'b001; // sub
                                 else
                                     ALUControl = 3'b000; // add, addi
                         3'b010: ALUControl = 3'b101; // slt, slti
-                        3'b100: ALUControl = 3'b100; // xor, xori (Senin ALU'na özel)
+                        3'b100: ALUControl = 3'b100; // xor, xori
                         3'b110: ALUControl = 3'b011; // or, ori
                         3'b111: ALUControl = 3'b010; // and, andi
                         default: ALUControl = 3'bxxx; // undefined
